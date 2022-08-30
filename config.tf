7e100ae8ee0e0b8e351da7d68e03d61658219f3e
tsvyak07 <tsvyake@gmail.com>
2022-08-30 22:54:07 +0500

    Initial commit

---
+++ config.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.20.2"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+}
