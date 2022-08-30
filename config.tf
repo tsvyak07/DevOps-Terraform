d3f691e6da0e6accee8fb826bf444e2ae94f22f9
tsvyak07 <tsvyake@gmail.com>
2022-08-30 23:16:31 +0500

    update tf scenario

--- config.tf
+++ config.tf
@@ -9,4 +9,20 @@ terraform {
 
 provider "docker" {
   # Configuration options
+  host    = "unix:///var/run/docker.sock"
+
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx-test"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 80
+    external = 80
+  }
+}
+
+# Find the latest Nginx precise image.
+resource "docker_image" "ubuntu" {
+  name = "nginx:latest"
 }
