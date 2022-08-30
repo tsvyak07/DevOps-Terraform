472a7651f3cb06e8a3cc86cd470af56d40c86bf8
tsvyak07 <tsvyake@gmail.com>
2022-08-30 23:53:31 +0500

    add new tf file provision new ec2 instance into to aws

---
+++ awsec2.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.28.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c9354388bb36c088"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
