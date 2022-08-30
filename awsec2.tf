b05816bac5004d71b252063f7b408f52b6d51bf1
tsvyak07 <tsvyake@gmail.com>
2022-08-31 01:08:45 +0500

    add new tf file provision new ec2 instance into to aws-2

--- awsec2.tf
+++ awsec2.tf
@@ -15,9 +15,13 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0c9354388bb36c088"
   instance_type = "t2.micro"
-  vpc_security_group_ids = "sg-090d33fde578e6e1f"
+
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  vpc_security_group_ids = "sg-090d33fde578e6e1f"
 }
